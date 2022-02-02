055c3b277c86eb9ff6e1c9f25ca394b56548d1d5
Brandon <kisibubrandon5@gmail.com>
2022-02-02 12:08:27 +0200

    completed map section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import NavbarBanner from '../components/NavbarBanner'
+import Map from '../components/Map'
 
 export default function Home() {
   return (
     <>
       <NavbarBanner />
+      <Map />
     </>
   )
 }
